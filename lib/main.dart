0aa7e854ca5b25879a523c263c3123edcdf44c46
David Lledó Andreu <lledoandreu84@gmail.com>
2026-08-11 15:56:26 +0200

    Initialize Supabase client

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'app/app.dart';
+import 'core/supabase/supabase_config.dart';
 
-void main() {
-  runApp(const ProviderScope(child: TruekeApp()));
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: SupabaseConfig.url,
+    anonKey: SupabaseConfig.anonKey,
+  );
+
+  runApp(
+    const ProviderScope(
+      child: TruekeApp(),
+    ),
+  );
 }
