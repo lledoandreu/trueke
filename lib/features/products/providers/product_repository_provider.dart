7f1267cd0a107dd94cd907e3846d74942ad9e278
David Lledó Andreu <lledoandreu84@gmail.com>
2026-09-08 23:19:07 +0200

    refactor: unificar inyeccion de dependencias de Supabase con Riverpod en proveedores globales y locales

--- lib/features/products/providers/product_repository_provider.dart
+++ lib/features/products/providers/product_repository_provider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:trueke/core/supabase/supabase_client.dart';
-import '../../features/products/repositories/product_repository.dart';
-import '../../features/products/repositories/supabase_product_repository.dart';
+import '../repositories/product_repository.dart';
+import '../repositories/supabase_product_repository.dart';
 
 final productRepositoryProvider = Provider<ProductRepository>((ref) {
   final client = ref.watch(supabaseClientProvider);
