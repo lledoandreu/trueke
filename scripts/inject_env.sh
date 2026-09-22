246395d5e8b4ea8ddc6abc6b68cb037a404bb431
lledoandreu <lledoandreu84@gmail.com>
2026-09-22 11:35:08 +0200

    chore: proteger y ocultar archivos de entorno locales

--- scripts/inject_env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-echo "SUPABASE_URL=https://supabase.co" > .env
-echo "SUPABASE_ANON_KEY=sb_publishable_imc2FiuFqqTu4z19_cV_PQ_kxpyCBmq" >> .env
-echo "SUPABASE_URL=https://supabase.co" > .env.production
-echo "SUPABASE_ANON_KEY=sb_publishable_imc2FiuFqqTu4z19_cV_PQ_kxpyCBmq" >> .env.production
